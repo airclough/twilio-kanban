461cb91e89bd2795f8f5713f8ab8d5eac94f85a4
Robby Fairclough <robby.fairclough@gmail.com>
2022-05-01 22:48:58 -0700

    query

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,42 @@
-import React, { FC } from 'react';
+import { gql, useLazyQuery } from '@apollo/client';
+import React, { FC, useEffect } from 'react';
 
 import DragAnDropContainer from '../src/components/DragAndDropContainer';
 import Footer from '../src/components/Footer';
 import Header from '../src/components/Header';
 
-const Index: FC = () => (
-  <div className="Index">
-    <Header />
-    <DragAnDropContainer />
-    <Footer />
-  </div>
-);
+const Index: FC = () => {
+  const tasksQuery = gql`
+    query ( $numTasks: Int! ) {
+      getTasks ( numTasks: $numTasks ) {
+        description,
+        id,
+        name,
+        status
+      }
+    }
+  `;
+  const [ getTasks, { data, error } ] = useLazyQuery( tasksQuery, { fetchPolicy: 'no-cache' } );
+
+  useEffect( () => {
+    const min = 8;
+    const max = 12;
+    const numTasks = Math.floor( Math.random() * ( max - min + 1 ) + min );
+
+    getTasks( { variables: { numTasks } } );
+  }, [] );
+
+  useEffect( () => {
+    console.log( { data, error } );
+  }, [ data, error ] );
+
+  return (
+    <div className="Index">
+      <Header />
+      <DragAnDropContainer />
+      <Footer />
+    </div>
+  );
+};
 
 export default Index;
--- src/services/apollo.ts
+++ src/services/apollo.ts
@@ -2,7 +2,8 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient( {
   cache: new InMemoryCache(),
-  uri: process.env.TWILIO_KANBAN_GRAPHQL_SERVER_URI,
+  headers: { 'x-twilio-kanban-graphql-server-auth-token': process.env.NEXT_PUBLIC_TWILIO_KANBAN_GRAPHQL_SERVER_AUTH_TOKEN },
+  uri: process.env.NEXT_PUBLIC_TWILIO_KANBAN_GRAPHQL_SERVER_URI,
 } );
 
 export default client;
